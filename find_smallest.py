08c79d043f25e781002532eb592e5b1aeb052700
BRAVEcrash <baiyshali39@gmail.com>
2026-01-22 15:56:49 +0900

    selection_sort

--- find_smallest.py
+++ find_smallest.py
@@ -1,7 +1,7 @@
 def find_smallest(arr):
     smallest = arr[0]
     smallest_index = 0
-    for i in range (1, len(arr)):
+    for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
             smallest_index = i
