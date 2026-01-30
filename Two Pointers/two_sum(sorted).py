dd420f0a2ef80a147f13ca68b86231da1c1951a1
BRAVEcrash <baiyshali39@gmail.com>
2026-01-30 14:50:08 +0900

    2pointers

---
+++ Two Pointers/two_sum(sorted).py
@@ -0,0 +1,12 @@
+def twoSumSorted(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        s = nums[left] + nums[right]
+        if s == target:
+            return [left, right]
+        elif s < target:
+            left += 1
+        else:
+            right -= 1
+     
