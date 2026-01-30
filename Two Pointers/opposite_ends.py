5a447d5f88e5b038e11185e330cf2ee2cdb4d38e
BRAVEcrash <baiyshali39@gmail.com>
2026-01-30 15:00:15 +0900

    2pointers

--- Two Pointers/opposite_ends.py
+++ Two Pointers/opposite_ends.py
@@ -1,9 +1,18 @@
 def opposite_ends(arr):
     left = 0
     right = len(arr) - 1
-    
+
     while left < right:
         if condition:
             left += 1
         else: 
             right -= 1
+'''
+🧠 Mental model
+👉 Pointers move toward each other
+🧪 Used for:
+• Sorted arrays
+• Pair sums
+• Palindromes
+• Reverse operations
+'''
---
+++ Two Pointers/same_direction(fastSlow).py
@@ -0,0 +1,6 @@
+slow = 0
+
+for fast in range(len(arr)):
+    if condition:
+        arr[slow] = arr[fast]
+        slow += 1
