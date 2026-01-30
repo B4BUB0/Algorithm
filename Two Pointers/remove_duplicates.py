d36616ee69f5e6cd2c263ebd554fcbb5cd73bbfe
BRAVEcrash <baiyshali39@gmail.com>
2026-01-30 15:04:46 +0900

    2pointers

---
+++ Two Pointers/remove_duplicates.py
@@ -0,0 +1,7 @@
+def removeDuplicates(nums):
+    slow = 0
+    for fast in range(len(nums)):
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow] = nums[fast]
+    return slow + 1
--- Two Pointers/same_direction(fastSlow).py
+++ Two Pointers/same_direction(fastSlow).py
@@ -4,3 +4,12 @@ for fast in range(len(arr)):
     if condition:
         arr[slow] = arr[fast]
         slow += 1
+        
+"""
+🧠 Mental model
+👉 One pointer explores, the other builds result
+🧪 Used for:
+• Removing duplicates
+• Filtering arrays
+• In-place operations
+"""
