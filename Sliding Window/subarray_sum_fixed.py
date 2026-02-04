0d7c960d5e72b8baf90fa720fd9ed784f2645a54
BRAVEcrash <baiyshali39@gmail.com>
2026-02-04 14:21:58 +0900

    subarray

---
+++ Sliding Window/subarray_sum_fixed.py
@@ -0,0 +1,11 @@
+def subarray_sum_fixed(nums, k):
+    window_sum = 0
+    for i in range(k):
+        window_sum += nums[i]
+    largest = window_sum
+    for right in range(k, len(nums)):
+        left = right - k
+        window_sum -= nums[left]
+        window_sum += nums[right]
+        largest = max(largest, window_sum)
+    return largest
