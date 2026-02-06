09a629f19f9e392ca9ddbe8efb82d2f6b7eb538c
BRAVEcrash <baiyshali39@gmail.com>
2026-02-06 12:04:36 +0900

    findMaxAverage

---
+++ Sliding Window/maximum_average_subarray.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findMaxAverage(self, nums, k):
+        window_sum = 0
+        for i in range(k):
+            window_sum += nums[i]
+        largest = window_sum
+
+        for right in range(k, len(nums)):
+            left = right - k 
+            window_sum -= nums[left]
+            window_sum += nums[right]
+            largest = max(largest, window_sum)
+
+        return largest / k
+    
+    # class Solution:
+#     def findMaxAverage(self, nums: List[int], k: int) -> float:
+#         window_sum = sum(nums[:k])
+#         largest = window_sum
+
+#         for right in range(k, len(nums)):
+#             left = right - k
+#             window_sum += nums[right] - nums[left]
+#             largest = max(largest, window_sum)
+
+#         return largest / k
+
+
+
+''''Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000'''
