a58f05192852e0a1cf89e9cf71bfe9a76389f101
BRAVEcrash <baiyshali39@gmail.com>
2026-02-09 16:06:30 +0900

    longestSubstring

--- Sliding Window/longest_substring.py
+++ Sliding Window/longest_substring.py
@@ -13,3 +13,13 @@ class Solution:
                 l += 1
             longest = max(longest, r - l + 1)
         return longest
+    
+''''Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000'''
