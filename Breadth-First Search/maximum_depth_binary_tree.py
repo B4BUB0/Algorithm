cd83610a9edad3b679c2f59ea260cec5092961c4
BRAVEcrash <baiyshali39@gmail.com>
2026-02-17 19:28:25 +0900

    Depth First Search

---
+++ Breadth-First Search/maximum_depth_binary_tree.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+def tree_max_depth(root: Node) -> int:
+    def dfs(root):
+        if not root:
+            return 0
+        return max(dfs(root.left), dfs(root.right))+ 1
+    return dfs(root) -1 if root else 0
+#this function build a tree from input:
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == "x":
+        return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+if __name__ == "__main__":
+    root = build_tree(iter(input().split()), int)
+    res = tree_max_depth(root)
+    print(res)
