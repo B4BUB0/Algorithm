b31da3bf74bbca6185e7372c29f1630628175bb6
BRAVEcrash <baiyshali39@gmail.com>
2026-02-21 12:26:47 +0900

    backtracking

--- Backtracking/Word_Search.py
+++ Backtracking/Word_Search.py
@@ -19,4 +19,16 @@ def exist(board, list):
                 return True
     return False
 
+"""Example 1:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+Example 2:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false"""
 
