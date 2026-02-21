365d4aad854a1a6f7d2fb48a703569900e361101
BRAVEcrash <baiyshali39@gmail.com>
2026-02-21 12:27:23 +0900

    backtracking

--- Backtracking/Word_Search.py
+++ Backtracking/Word_Search.py
@@ -26,9 +26,8 @@ Output: true
 Example 2:
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
 Output: true
+
 Example 3:
-
-
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
 Output: false"""
 
