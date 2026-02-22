fa45b6accc238392293bc9ae806ddabbaf7d30f2
BABUBO <102480883+B4BUB0@users.noreply.github.com>
2026-02-22 11:19:25 +0900

    Fix formatting of example input in Word_Search.py

--- Backtracking/Word_Search.py
+++ Backtracking/Word_Search.py
@@ -30,6 +30,7 @@ word = "SEE"
 Output: true
 
 Example 3:
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 
+word = "ABCB"
 Output: false"""
 
